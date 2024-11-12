cb6c55bf4dc0b16b70b7aa6cce04a4d7d7122fac
Gustavo Rahmeier <90331005+gustavorah@users.noreply.github.com>
2024-11-11 21:38:00 -0300

    first commit

---
+++ api-gateway/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('users')->group(function () {
+    Route::get('/', function () {
+        return 'dsfsd';
+    });
+    Route::post('/', function () {
+        return 'ffff';
+    });
+    Route::get('/{user}', function () {
+        return 'asd';
+    });
+    Route::put('/edit', function () {
+        return 'sadas';
+    });
+});
